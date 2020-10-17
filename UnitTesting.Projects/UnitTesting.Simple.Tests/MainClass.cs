065ffa561a956517bd1336982f7b1a7471745d26
Roberto Febrer <febrer@gmail.com>
2020-10-17 22:02:16 +0200

    Initial commit, adding simple project

---
+++ UnitTesting.Projects/UnitTesting.Simple.Tests/MainClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UnitTesting.Simple.Tests
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.ReadLine();
+                Console.WriteLine("Starting");
+                SimpleParserTests.TestReturnsZeroWhenEmptyString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ UnitTesting.Projects/UnitTesting.Simple.Tests/TestUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UnitTesting.Simple.Tests
+{
+    public class TestUtil
+    {
+        public static void ShowProblem(string test, string message)
+        {
+            string msg = string.Format(@"
+---{0}---
+       {1}
+--------------------
+", test, message);
+            Console.WriteLine(msg);
+        }
+    }
+}
